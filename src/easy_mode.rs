94c51153586eb740b219a7d140f28f06b1424571
9names <60134748+9names@users.noreply.github.com>
2023-09-13 22:20:10 +1000

    Add function for finding and decoding id3v2 header

--- src/easy_mode.rs
+++ src/easy_mode.rs
@@ -22,7 +22,7 @@ impl EasyMode {
     }
 
     /// Add MP3 data to the EasyMode internal MP3 stream buffer
-    /// This function will also attempt
+    /// This function will also attempt to find the start of stream
     pub fn add_data(&mut self, data: &[u8]) -> usize {
         let bytes_added = self.buffer.load_slice(data);
         if !self.sync {
@@ -33,6 +33,7 @@ impl EasyMode {
                 // Also try to get frame info for next frame
                 let f = self.mp3.get_next_frame_info(self.buffer.borrow_slice());
                 if f.is_ok() {
+                    // we should check *two* frames to be very sure, but leave it at one for now.
                     self.have_decoded = true;
                 }
             } else {
@@ -44,6 +45,11 @@ impl EasyMode {
         bytes_added
     }
 
+    /// Add MP3 data to the EasyMode internal MP3 stream buffer
+    pub fn add_data_no_sync(&mut self, data: &[u8]) -> usize {
+        self.buffer.load_slice(data)
+    }
+
     /// How much data is free in the EasyMode internal MP3 stream buffer
     pub fn buffer_free(&self) -> usize {
         self.buffer.available()
@@ -52,6 +58,13 @@ impl EasyMode {
     /// How much MP3 data is in the EasyMode internal MP3 stream buffer
     pub fn buffer_used(&self) -> usize {
         self.buffer.used()
+    }
+
+    /// Skip over data in the buffer without decoding it
+    pub fn buffer_skip(&mut self, count: usize) -> usize {
+        let to_remove = core::cmp::min(self.buffer.used(), count);
+        self.buffer.increment_start(to_remove);
+        to_remove
     }
 
     /// Decode the next MP3 audio frame after checking that the output buffer is large enough
@@ -113,6 +126,28 @@ impl EasyMode {
             let frame = self.mp3.get_next_frame_info(self.buffer.borrow_slice())?;
             Ok(frame)
         }
+    }
+
+    // from https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-structure.html
+    // ID3 tag format is as follows
+    // $49 44 33 yy yy xx zz zz zz zz
+    // yy yy is the version, xx is flags, zz zz zz zz is the ID3v2 tag size.
+    //
+    /// Find and decode ID3v2 header info
+    pub fn find_id3v2(&mut self) -> Option<(usize, u8, u8, u8, usize)> {
+        let window = self.buffer.borrow_slice().windows(10);
+        for (offset, slice) in window.enumerate() {
+            if let [b'I', b'D', b'3', major, minor, flags, s1, s2, s3, s4] = slice {
+                let (s1, s2, s3, s4) = (*s1 as usize, *s2 as usize, *s3 as usize, *s4 as usize);
+                // The ID3v2 tag size is stored as a 32 bit synchsafe integer, making a total of 28 effective bits (representing up to 256MB).
+                // a syncsafe integer is a 7bit integer where the top bit is always zero.
+                if (s1 | s2 | s3 | s4) & 0b1000_0000 != 0b1000_0000 {
+                    let size = s4 | s3 << 7 | s2 << 14 | s1 << 21;
+                    return Some((offset, *major, *minor, *flags, size));
+                }
+            }
+        }
+        None
     }
 }
 
